fe36b0d8db05a503bd16d153a386017ada4fd1a3
Prengelshausen <rengelshausen.business@gmail.com>
2023-01-21 19:13:37 +0000

    styled card component, created modal component that shows more details

--- client_pokestarter/src/App.vue
+++ client_pokestarter/src/App.vue
@@ -1,35 +1,59 @@
 <template>
+  <div>Pokemon Starter of every Generation</div>
+  <!-- <div v-if="showing">
+    Modal here.
+  </div> -->
+
+  <ModalComponent
+    :showing="showing"
+    :currpkmn="currpkmn"
+    @close="showing = false"
+    class="fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
+  />
+
   <div class="flex flex-wrap justify-center container mx-auto w-full">
-    <CardComponent :pokestarter="pokestarter" />
+    <CardComponent
+      @clickCard="clickCard"
+      :pokestarter="pokestarter"
+      :showing="showing"
+    />
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import CardComponent from '@/components/CardComponent.vue';
+import CardComponent from "@/components/CardComponent.vue";
+import ModalComponent from "@/components/ModalComponent.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    CardComponent
+    CardComponent,
+    ModalComponent,
   },
   data() {
     return {
-      pokestarter: []
-    }
+      pokestarter: [],
+      showing: false,
+      currpkmn: "",
+    };
   },
   methods: {
-
+    clickCard(pkmn) {
+      this.currpkmn = pkmn;
+      this.showing = true;
+      console.log(this.currpkmn);
+    },
   },
-  async created () {
+  async created() {
     try {
       let res = await axios.get("http://localhost:3000/pokestarter/");
       this.pokestarter = res.data.data;
       console.log(this.pokestarter);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   },
-}
+};
 </script>
 
 <style>
